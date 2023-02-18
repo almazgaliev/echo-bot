a509ab70fb36342186b044ee94318ebfc9216f7f
almazgaliev <almazgaliev99@gmail.com>
2023-02-18 20:23:51 +0300

    wip: appendmode for logs

--- src/Config.hs
+++ src/Config.hs
@@ -11,11 +11,9 @@ where
 import ConfigurationTypes qualified
 import Data.Maybe (fromJust)
 import Data.Text qualified as T
-import Debug.Trace (traceShow)
 import EchoBot qualified
-import Logger (Level (Debug))
 import Logger.Impl qualified
-import System.IO (IOMode (WriteMode), openFile)
+import System.IO (IOMode (..), openFile)
 import Text.JSON
 
 {- | Gets the bot config. In any case it can provide reasonable
@@ -36,7 +34,7 @@ getLoggerConfig = do
   let config = fromJSObject configJSObject -- FIX later handle errors
   let logLevel = read . fromJust . lookup "minLevel" $ config
   let path = fromJust . lookup "file" $ config
-  handle <- openFile path WriteMode
+  handle <- openFile path AppendMode
   return $
     Logger.Impl.Config
       { Logger.Impl.confFileHandle = handle
