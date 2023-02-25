e5a2502fcd17386a9a1aa8fc8e4dd4a27ec0c6f3
almazgaliev <almazgaliev99@gmail.com>
2023-02-25 21:28:26 +0300

    feat: added telegram API methods

---
+++ src/TelegramAPI.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module TelegramAPI (
+  sendMessage,
+  getUpdates,
+) where
+
+import Data.ByteString.Char8 (pack)
+import Data.ByteString.Lazy.Internal (ByteString)
+import Network.HTTP.Client (Manager, Response, httpLbs, newManager, parseRequest)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Network.HTTP.Simple (
+  getResponseBody,
+  getResponseHeader,
+  getResponseStatusCode,
+  parseRequest,
+  setRequestBodyURLEncoded,
+  setRequestMethod,
+ )
+
+-- manager <- newManager tlsManagerSettings
+sendMessage :: Manager -> String -> String -> Int -> IO (Response ByteString)
+sendMessage manager token message chatId = do
+  let echoBody =
+        [ ("chat_id", pack . show $ chatId)
+        , ("text", pack message)
+        ]
+  request <- parseRequest $ "https://api.telegram.org/bot" ++ token ++ "/sendMessage"
+  let request' =
+        setRequestMethod "POST" $
+          setRequestBodyURLEncoded echoBody request
+  httpLbs request' manager
+
+getUpdates :: Manager -> String -> IO (Response ByteString)
+getUpdates manager token = do
+  request <- parseRequest $ "https://api.telegram.org/bot" ++ token ++ "/getUpdates"
+  let request' = setRequestMethod "POST" request
+  httpLbs request' manager
