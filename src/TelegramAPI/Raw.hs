42f765e26201676d2136a84c4f111c3b9ac2ddfb
almazgaliev <almazgaliev99@gmail.com>
2023-03-07 22:06:03 +0300

    refactor: telegram api

--- src/TelegramAPI/Raw.hs
+++ src/TelegramAPI/Raw.hs
@@ -1,36 +1,50 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module TelegramAPI.Raw (
+module TelegramAPI (
   sendMessage,
   getUpdates,
+  UpdateParams (..),
+  APIToken,
 ) where
 
-import Data.ByteString.Char8 (pack)
-import Data.ByteString.Lazy.Internal (ByteString)
-import Network.HTTP.Client (Manager, Response, httpLbs, parseRequest)
+import qualified Data.ByteString.Internal as BS (ByteString, packChars)
+import qualified Data.ByteString.Lazy.Internal as LBS (ByteString)
+import qualified Network.HTTP.Conduit as Conduit (Manager, Response, httpLbs, parseRequest)
 
-import Network.HTTP.Simple (
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import qualified Data.Word as Word (Word64)
+import qualified Network.HTTP.Simple as Simple (
   setRequestBodyURLEncoded,
   setRequestMethod,
  )
 
+data UpdateParams = UpdateParams {getOffset :: Word.Word64, allowedUpdates :: [String]}
+
+paramsToBody :: UpdateParams -> [(BS.ByteString, BS.ByteString)]
+paramsToBody params = [("offset", BS.packChars . show $ getOffset params)]
+
+type APIToken = String
 apiURL :: String
 apiURL = "https://api.telegram.org/"
 
-sendMessage :: Manager -> String -> String -> Int -> IO (Response ByteString)
+sendMessage :: Conduit.Manager -> APIToken -> String -> Word.Word64 -> IO (Conduit.Response LBS.ByteString)
 sendMessage manager token message chatId = do
-  let echoBody =
-        [ ("chat_id", pack . show $ chatId)
-        , ("text", pack message)
+  let body =
+        [ ("chat_id", BS.packChars . show $ chatId)
+        , ("text", T.encodeUtf8 . T.pack $ message)
         ]
-  request <- parseRequest $ apiURL ++ "bot" ++ token ++ "/sendMessage"
+  request <- Conduit.parseRequest $ apiURL ++ "bot" ++ token ++ "/sendMessage"
   let request' =
-        setRequestMethod "POST" $
-          setRequestBodyURLEncoded echoBody request
-  httpLbs request' manager
+        Simple.setRequestMethod "POST" $
+          Simple.setRequestBodyURLEncoded body request
+  Conduit.httpLbs request' manager
 
-getUpdates :: Manager -> String -> IO (Response ByteString)
-getUpdates manager token = do
-  request <- parseRequest $ apiURL ++ "bot" ++ token ++ "/getUpdates"
-  let request' = setRequestMethod "POST" request
-  httpLbs request' manager
+getUpdates :: Conduit.Manager -> APIToken -> UpdateParams -> IO (Conduit.Response LBS.ByteString)
+getUpdates manager token params = do
+  let body = paramsToBody params
+  request <- Conduit.parseRequest $ apiURL ++ "bot" ++ token ++ "/getUpdates"
+  let request' =
+        Simple.setRequestMethod "POST" $
+          Simple.setRequestBodyURLEncoded body request
+  Conduit.httpLbs request' manager
