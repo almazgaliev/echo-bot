4021c2ff84acb9898b426d9a4e073240cfff1ce9
almazgaliev <almazgaliev99@gmail.com>
2023-03-07 22:06:29 +0300

    wip : added wrapper types for telegram api

--- src/TelegramAPI/Wrappers.hs
+++ src/TelegramAPI/Wrappers.hs
@@ -1,36 +1,54 @@
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module TelegramAPI.Wrappers (getUpdates, UpdatesInfo (..), UpdateInfo (..), MessageInfo (..), ChatInfo (..))
+module TelegramAPI.Wrappers (getUpdates, SenderInfo (..), UpdatesInfo (..), UpdateInfo (..), MessageInfo (..), ChatInfo (..))
 where
 
-import Data.Aeson (FromJSON (parseJSON), eitherDecode, withObject, (.:))
-import GHC.Generics (Generic)
-import Network.HTTP.Client (Manager, Response (responseBody, responseStatus))
-import Network.HTTP.Types (Status (..))
-import qualified TelegramAPI.Raw as TAPI
+import Data.Aeson ((.:), (.:?))
+import qualified Data.Aeson as Aeson (FromJSON (parseJSON), eitherDecode, withObject)
+import qualified Data.Text as T
+import qualified Data.Word as Word (Word64)
+import qualified Network.HTTP.Conduit as Conduit
+import Network.HTTP.Types (Status (statusCode))
+import qualified TelegramAPI
 
-newtype ChatInfo = ChatInfo {id :: Integer} deriving (Show, Generic, FromJSON)
+newtype ChatInfo = ChatInfo {getChatId :: Word.Word64} deriving (Show)
 
-data MessageInfo = MessageInfo {chatInfo :: ChatInfo, text :: String} deriving (Show)
+instance Aeson.FromJSON ChatInfo where
+  parseJSON = Aeson.withObject "ChatInfo" $ \v -> ChatInfo <$> v .: "id"
 
-instance FromJSON MessageInfo where
-  parseJSON = withObject "MessageInfo" $ \v -> MessageInfo <$> v .: "chat" <*> v .: "text"
+data SenderInfo = SenderInfo
+  { getUserId :: Word.Word64
+  , getFirstName :: String
+  , getUserName :: String
+  }
+  deriving (Show)
 
-data UpdateInfo = UpdateInfo {updateId :: Integer, message :: MessageInfo} deriving (Show)
+instance Aeson.FromJSON SenderInfo where
+  parseJSON = Aeson.withObject "SenderInfo" $ \v ->
+    SenderInfo <$> v .: "id" <*> v .: "first_name" <*> v .: "username"
 
-instance FromJSON UpdateInfo where
-  parseJSON = withObject "UpdateInfo" $ \v -> UpdateInfo <$> v .: "update_id" <*> v .: "message"
+data MessageInfo = MessageInfo {getChatInfo :: ChatInfo, getSender :: SenderInfo, getText :: Maybe T.Text} deriving (Show)
 
-data UpdatesInfo = UpdatesInfo {ok :: Bool, result :: [UpdateInfo]} deriving (Show, Generic, FromJSON)
+instance Aeson.FromJSON MessageInfo where
+  parseJSON = Aeson.withObject "MessageInfo" $ \v ->
+    MessageInfo <$> v .: "chat" <*> v .: "from" <*> v .:? "text"
 
-getUpdates :: Manager -> String -> IO (Either String UpdatesInfo)
-getUpdates manager token = do
-  response <- TAPI.getUpdates manager token
-  let code = statusCode . responseStatus $ response
-  let body = responseBody response
+data UpdateInfo = UpdateInfo {getUpdateId :: Word.Word64, getMessage :: Maybe MessageInfo} deriving (Show)
+
+instance Aeson.FromJSON UpdateInfo where
+  parseJSON = Aeson.withObject "UpdateInfo" $ \v -> UpdateInfo <$> v .: "update_id" <*> v .:? "message"
+
+data UpdatesInfo = UpdatesInfo {ok :: Bool, getResult :: [UpdateInfo]} deriving (Show)
+
+instance Aeson.FromJSON UpdatesInfo where
+  parseJSON = Aeson.withObject "UpdatesInfo" $ \v -> UpdatesInfo <$> v .: "ok" <*> v .: "result"
+
+getUpdates :: Conduit.Manager -> String -> TelegramAPI.UpdateParams -> IO (Either String UpdatesInfo)
+getUpdates manager token params = do
+  response <- TelegramAPI.getUpdates manager token params
+  let code = statusCode . Conduit.responseStatus $ response
+  let body = Conduit.responseBody response
   return $
     if code == 200
-      then eitherDecode body
+      then Aeson.eitherDecode body
       else Left $ "response code: " ++ show code
