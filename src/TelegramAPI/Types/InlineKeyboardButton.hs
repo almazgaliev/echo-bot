acfef8f6fbff815616a5290a456ce7ab40f027bf
almazgaliev <almazgaliev99@gmail.com>
2023-03-11 09:54:08 +0300

    feat: implemented InlineKeyboardButton

--- src/TelegramAPI/Types/InlineKeyboardButton.hs
+++ src/TelegramAPI/Types/InlineKeyboardButton.hs
@@ -1,11 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module TelegramAPI.Types.InlineKeyboardButton (InlineKeyboardButton) where
+module TelegramAPI.Types.InlineKeyboardButton (InlineKeyboardButton, mkInlineCallBackButton) where
 
-import Data.Aeson (KeyValue ((.=)), object)
+import Data.Aeson ((.=))
 import qualified Data.Aeson as Aeson
 
-newtype InlineKeyboardButton = InlineKeyboardButton {getText :: String} deriving (Show)
+data RequiredField = CallBack String | Url String deriving (Show)
+
+data InlineKeyboardButton = InlineKeyboardButton {getText :: Maybe String, requieredField :: RequiredField} deriving (Show)
 
 instance Aeson.ToJSON InlineKeyboardButton where
-  toJSON (InlineKeyboardButton {getText = text}) = object ["text" .= text]
+  toJSON (InlineKeyboardButton {getText = text, requieredField = (CallBack callback)}) = Aeson.object ["text" .= text, "callback_data" .= callback]
+  toJSON _ = error "Not Implemented"
+
+mkInlineCallBackButton :: String -> String -> InlineKeyboardButton
+mkInlineCallBackButton label data' =
+  InlineKeyboardButton
+    { getText = pure label
+    , requieredField = CallBack data'
+    }
