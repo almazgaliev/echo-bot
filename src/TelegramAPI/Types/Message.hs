057c3d09bd3f73575ed298a3f41486405aa21da6
almazgaliev <almazgaliev99@gmail.com>
2023-03-11 09:54:21 +0300

    refactor

--- src/TelegramAPI/Types/Message.hs
+++ src/TelegramAPI/Types/Message.hs
@@ -1,4 +1,5 @@
-module TelegramAPI.Types.Message (Message(..)) where
+module TelegramAPI.Types.Message (Message (..)) where
+
 import qualified TelegramAPI.Types.Markup as Markup
 
 data Message = Message {getText :: Maybe String, getMarkup :: Maybe Markup.Markup}
